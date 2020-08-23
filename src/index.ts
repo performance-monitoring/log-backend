9267c1ef2cce826a9b4ecf2785d0a6db73b5e4fb
treeandgrass <mymymymypcy@163.com>
2020-08-23 23:55:24 +0800

    feat: add routes

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,16 @@
 import * as koa from "koa"
 import * as bodyParser from "koa-bodyparser"
-import signale = require("signale");
+import * as signale from "signale"
+import router from './routes/route'
 import { appLog } from "./utils/log"
 
 const PORT = 8080
-
 const app = new koa();
 
-appLog.log("hello")
-
-app.use(bodyParser());
+app.use(bodyParser())
+app.use(router.routes())
 
 app.listen(PORT, () => {
+  appLog.log(`server start at ${PORT}`)
   signale.info(`server start at ${PORT}`)
 })
---
+++ src/routes/route.ts
@@ -0,0 +1,9 @@
+import * as Router from "koa-router"
+
+const router = new Router()
+
+router.get('/query', (ctx) => {
+  //
+})
+
+export default router
